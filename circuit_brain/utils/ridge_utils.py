d1ed5decab0ec0b8ef6cca038ea230fdb28303ce
alansun17904 <asun17904@gmail.com>
2024-06-28 10:45:05 +0200

    gpu support for regression

--- circuit_brain/utils/ridge_utils.py
+++ circuit_brain/utils/ridge_utils.py
@@ -20,29 +20,30 @@ def r2r_score(pred, actual):
 def ridge(x, y, lam):
     nfeats = x.shape[1]
     return torch.matmul(
-        tla.inv(torch.matmul(x.T, x) + lam * torch.eye(nfeats)), torch.matmul(x.T, y)
+        tla.inv(torch.matmul(x.T, x) + lam * torch.eye(nfeats, device=x.device)),
+        torch.matmul(x.T, y),
     )
 
 
 def ridge_lam_per_target(x, y, x_val, y_val, lams=[1e-4, 1e-3]):
-    error = torch.zeros(len(lams), y.shape[1])
+    error = torch.zeros(len(lams), y.shape[1], device=y.device)
     for i, lam in enumerate(lams):
         weights = ridge(x, y, lam)
         error[i] = 1 - r2_score(torch.matmul(x_val, weights), y_val)
     return error
 
 
-def cross_val_ridge(x_train, y_train, n_splits=10, lams=[1e-3, 1e-4]):
-    r_cv = torch.zeros(len(lams), y_train.shape[1])
+def cross_val_ridge(x_train, y_train, n_splits=5, lams=[1e-3, 1e-4,1e-5]):
+    r_cv = torch.zeros(len(lams), y_train.shape[1], device=y_train.device)
     kf = KFold(n_splits=n_splits)
     for f, (t_idx, v_idx) in enumerate(kf.split(y_train)):
         fx_train, fy_train = x_train[t_idx], y_train[t_idx]
         fx_val, fy_val = x_train[v_idx], y_train[v_idx]
         r_cv += ridge_lam_per_target(fx_train, fy_train, fx_val, fy_val, lams=lams)
     min_lams = torch.argmin(r_cv, axis=0)
-    weights = torch.zeros((x_train.shape[1], y_train.shape[1]))
-
+    weights = torch.zeros((x_train.shape[1], y_train.shape[1]), device=x_train.device)
     for i in range(len(lams)):
-        y_label_idx = min_lams == i
-        weights[:, y_label_idx] = ridge(x_train, y_train[:, y_label_idx], lams[i])
-    return weights, np.array([lams[i] for i in min_lams])
+        mask = min_lams == i
+        if torch.any(mask):
+            weights[:, mask] = ridge(x_train, y_train[:, mask], lams[i])
+    return weights #, np.array([lams[i] for i in min_lams])
